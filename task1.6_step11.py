a82eca9a43f857b045ccdc3d0e0fa218c214fd40
Ilnur <https://github.com/ilnur2242/MarkvardetMethod.git>
2022-05-25 23:47:09 +0300

    task 1.6 step 11

---
+++ task1.6_step11.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+import time
+
+link = 'http://suninjuly.github.io/registration1.html'
+#link = 'http://suninjuly.github.io/registration2.html' # bug
+try: 
+	browser = webdriver.Chrome() #executable_path=r'D:\GoogleDrive\chromedriver.exe'
+	browser.get(link)
+
+	# Ваш код, который заполняет обязательные поля
+	input1 = browser.find_element_by_css_selector(".first_block .first")
+	input1.send_keys('il')
+
+	input1 = browser.find_element_by_css_selector(".first_block .second")
+	input1.send_keys('val')
+
+	input1 = browser.find_element_by_css_selector(".first_block .third")
+	input1.send_keys('il@fake.com')
+
+
+	# Отправляем заполненную форму
+	button = browser.find_element_by_css_selector("button.btn")
+	button.click()
+
+	# Проверяем, что смогли зарегистрироваться
+	# ждем загрузки страницы
+	time.sleep(1)
+
+	# находим элемент, содержащий текст
+	welcome_text_elt = browser.find_element_by_tag_name("h1")
+	# записываем в переменную welcome_text текст из элемента welcome_text_elt
+	welcome_text = welcome_text_elt.text
+
+	# с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+	assert "Congratulations! You have successfully registered!" == welcome_text
+
+except Exception as error:
+    print(f'Произошла ошибка, вот её трэйсбэк: {error}')
+finally:
+	# ожидание чтобы визуально оценить результаты прохождения скрипта
+	time.sleep(10)
+	# закрываем браузер после всех манипуляций
+	browser.quit()

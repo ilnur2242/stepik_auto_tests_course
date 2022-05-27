9f5a9eb69c35bf08216bdd476dacee4ea56a50e0
Ilnur <https://github.com/ilnur2242/MarkvardetMethod.git>
2022-05-27 18:50:48 +0300

    save lesson3_6 code

---
+++ lesson3_6/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+executable_path=r'D:\GoogleDrive\chromedriver.exe'
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language. For example: ru,es,fr")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    browser = None
+    if user_language:
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        print("\nstart browser for test..")
+        browser = webdriver.Chrome(options=options)
+    else:
+        raise pytest.UsageError('--language not set')
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
---
+++ lesson3_6/test_items.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+import time
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_option_to_add_offer_to_cart(browser):
+	browser.get(link)
+	time.sleep(10)
+
+	basket_button = browser.find_element(By.CSS_SELECTOR,'button.btn-add-to-basket')
+	
+	assert basket_button, 'add_to_cart_button does not exist'

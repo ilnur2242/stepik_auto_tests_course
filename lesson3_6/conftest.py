0a4e78bbdab69097a7f8ee57c6f916cc97f5a68c
Ilnur <https://github.com/ilnur2242/MarkvardetMethod.git>
2022-05-27 18:53:46 +0300

    edit lesson3_6 code. clear rubbish

--- lesson3_6/conftest.py
+++ lesson3_6/conftest.py
@@ -1,9 +1,6 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-
-executable_path=r'D:\GoogleDrive\chromedriver.exe'
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
--- lesson3_6/test_items.py
+++ lesson3_6/test_items.py
@@ -5,7 +5,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_option_to_add_offer_to_cart(browser):
 	browser.get(link)
-	time.sleep(10)
+	#time.sleep(10)
 
 	basket_button = browser.find_element(By.CSS_SELECTOR,'button.btn-add-to-basket')
 	
